be634b755c4cccbd6379257d119df84d53d967d9
Anothernaso <antondev@kuthy.com>
2026-01-13 16:09:12 +0100

    Add new level and improve documentation

--- scripts/arrow/arrow_sequence.gd
+++ scripts/arrow/arrow_sequence.gd
@@ -2,6 +2,13 @@ extends Resource
 
 class_name ArrowSequence
 
+## The delay from the beginning of the timeline to when
+## the arrows should start spawning.
 @export var min_survival_time: int = 0
+
+## The delay from the beginning of the timeline to when
+## the arrows should stop spawning.
 @export var max_survival_time: int = 100
+
+## The arrow scene that should be spawned in this sequence.
 @export var scene: PackedScene
