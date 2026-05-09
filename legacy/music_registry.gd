b5cd651262c806c394cce3f5432888667a18e21f
Anatnaso <antondev@kuthy.com>
2026-05-09 09:46:13 +0200

    Remove legacy code
    On branch master
    Changes to be committed:
    	deleted:    content/albums/legacy/eternity.tres
    	deleted:    legacy/music_registry.gd
    	deleted:    legacy/music_registry.gd.uid
    	deleted:    legacy/projectile_zone/projectile_zone.gd
    	deleted:    legacy/projectile_zone/projectile_zone.gd.uid
    	deleted:    legacy/projectile_zone/projectile_zone.tscn
    	deleted:    legacy/root.gd
    	deleted:    legacy/root.gd.uid
    	deleted:    legacy/root.tscn

--- legacy/music_registry.gd
+++
@@ -1,8 +0,0 @@
-# A MusicRegistry is basically like
-# an album of music that the Music class
-# can play.
-
-class_name MusicRegistry
-extends Resource
-
-@export var music_streams: Array[AudioStream] = []
--- legacy/projectile_zone/projectile_zone.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-func _on_body_exited(body: Node2D) -> void:
-	if !body.is_in_group("projectile"): return
-	
-	body.queue_free()
-	
