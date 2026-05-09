0af51aaa93161b1c15523d6b14780410c910051e
Anatnaso <antondev@kuthy.com>
2026-05-09 11:13:11 +0200

    Rework internal UI layout of `death_screen.tscn`.
    On branch master
    Changes to be committed:
    	modified:   scenes/death_screen/death_screen.gd
    	modified:   scenes/death_screen/death_screen.tscn
    	modified:   scenes/main_menu/main_menu.tscn

--- scenes/death_screen/death_screen.gd
+++ scenes/death_screen/death_screen.gd
@@ -4,22 +4,22 @@ class_name DeathScreen
 
 @export var album: MusicAlbum
 
-@onready var score_counter_label: Label = $UI/Control/Panel/ScoreCounter
-@onready var highscore_counter_label: Label = $UI/Control/Panel/HighscoreCounter
+@onready var score_label: Label = $UI/Control/CenterContainer/Panel/VSplitContainer/VBoxContainer/ScoreLabel
+@onready var highscore_label: Label = $UI/Control/CenterContainer/Panel/VSplitContainer/VBoxContainer/HighscoreLabel
 
 func _ready() -> void:
 	var music_manager: AD_MusicManager = AD_GlobalMusicManager
 	music_manager.set_album(album, true)
 	
-	score_counter_label.text = "Time survived: " + FormatUtils.format_time(ScoreCounter.last_time_survived)
-	highscore_counter_label.text = "Highscore: " + FormatUtils.format_time(ScoreCounter.highscore_time_survived)
+	score_label.text = "Time Survived: " + FormatUtils.format_time(ScoreCounter.last_time_survived)
+	highscore_label.text = "Highscore: " + FormatUtils.format_time(ScoreCounter.highscore_time_survived)
 	
 
-func _on_try_again_button_down() -> void:
+func _on_retry_button_down() -> void:
 	var game_state_manager: AD_GameStateManager = AD_GlobalGameStateManager
 	game_state_manager.set_state(GameStates.State.InGame)
 
 
-func _on_main_menu_button_button_down() -> void:
+func _on_main_menu_button_down() -> void:
 	var game_state_manager: AD_GameStateManager = AD_GlobalGameStateManager
 	game_state_manager.set_state(GameStates.State.MainMenu)
