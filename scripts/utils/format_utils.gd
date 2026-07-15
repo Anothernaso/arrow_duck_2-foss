5ed81739a6cedbca0be72ebfe0ba36e7df336d7c
Anatnaso <antondev@kuthy.com>
2026-07-15 10:50:48 +0200

    Fix warnings in `format_time` function and fix physics interpolation waring in `player_camera.tscn`
    Changes to be committed:
    	modified:   nodes/player/player_camera.tscn
    	modified:   scripts/utils/format_utils.gd

--- scripts/utils/format_utils.gd
+++ scripts/utils/format_utils.gd
@@ -2,10 +2,10 @@ class_name AD_FormatUtils
 
 ## Fancy time formatter that produces readable timestamps
 static func format_time(seconds: float) -> String:
-	var total_seconds := int(seconds + 0.5)
+	var total_seconds := roundi(seconds) # Round it properly or something; IDK
 	
-	var hours := total_seconds / 3600
-	var minutes := (total_seconds % 3600) / 60
+	var hours := int(float(total_seconds) / 3600)
+	var minutes := int(float(total_seconds % 3600) / 60)
 	var remaining_seconds := total_seconds % 60
 
 	var time_str := ""
@@ -16,4 +16,4 @@ static func format_time(seconds: float) -> String:
 	time_str += str(minutes).pad_zeros(2) + "m "
 	time_str += str(remaining_seconds).pad_zeros(2) + "s"
 	
-	return time_str.strip_edges() # Removes any trailing spaces
+	return time_str.strip_edges() # Remove any trailing spaces
